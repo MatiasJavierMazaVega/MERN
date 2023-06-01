f61859f2ca9289326e76d7e779f4ec11662b55c6
MatiasMV <mjmazavega@gmail.com>
2023-06-01 17:39:29 -0300

    first commit

---
+++ model/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const userSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    apellido: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+//exportamos la configuracion con el nombre de la coleccion
+module.exports = mongoose.model('Formularios', userSchema);
